5f53159baf666c065ea3671018586a871a8503e5
Nat! <nat@codeon.de>
2016-09-06 16:17:12 +0200

    adapt to mulle-tests

---
+++ tests/build-test.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+LIBRARY_SHORTNAME="mulle-utf"
+
+MAKE=make  # nmake not good
+
+. "../mulle-tests/test-c-common.sh"
+. "../mulle-tests/test-tools-common.sh"
+. "../mulle-tests/build-test-common.sh"
