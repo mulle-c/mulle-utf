b4acb6f5f1ff750f3841d5b4d066420801cbc60f
Nat! <nat@mulle-kybernetik.com>
2017-06-14 15:37:44 +0200

    Use templates to create .md files where necessary

--- bin/formula-info.sh
+++ bin/formula-info.sh
@@ -1,3 +1,3 @@
-PROJECT="MulleUtf"   # your project name, requires camel-case
+PROJECT="mulle-utf"   # your project name, requires camel-case
 DESC="UTF8-16-32 analysis, conversion, classification library"
 DEPENDENCIES='${DEPENDENCY_TAP}mulle-allocator'
