86fb3dec39f0acc5330d366a883071073e63e48e
Nat! <nat@mulle-kybernetik.com>
2021-11-12 00:17:56 +0100

    update mulle-sde, add .gitattributes if needed

--- src/reflect/_mulle-utf-include.h
+++ src/reflect/_mulle-utf-include.h
@@ -16,26 +16,26 @@
 
 // You can tweak the following #include with these commands.
 // (Use 4CDE68EB-07DD-45DE-8DBB-2FD112F42C18 instead of mulle-allocator if there are duplicate entries)
-//    remove:             `mulle-sourcetree mark mulle-allocator no-header`
+//    remove:             `mulle-sde dependency mark mulle-allocator no-header`
 //    rename:             `mulle-sde dependency|library set mulle-allocator include whatever.h`
-//    reorder:            `mulle-sourcetree move mulle-allocator <up|down>`
-//    toggle #include:    `mulle-sourcetree mark mulle-allocator [no-]import`
-//    toggle public:      `mulle-sourcetree mark mulle-allocator [no-]public`
-//    toggle optional:    `mulle-sourcetree mark mulle-allocator [no-]require`
-//    remove for platform:`mulle-sourcetree mark mulle-allocator no-platform-<uname>`
-//        (use mulle-sourcetree-to-c --unames for known values)
+//    reorder:            `mulle-sde dependency move mulle-allocator <up|down>`
+//    toggle #include:    `mulle-sde dependency mark mulle-allocator [no-]import`
+//    toggle public:      `mulle-sde dependency mark mulle-allocator [no-]public`
+//    toggle optional:    `mulle-sde dependency mark mulle-allocator [no-]require`
+//    remove for platform:`mulle-sde dependency mark mulle-allocator no-platform-<uname>`
+//        (use `mulle-sourcetree-to-c --unames` to list known values)
 #include <mulle-allocator/mulle-allocator.h>   // mulle-allocator
 
 // You can tweak the following #include with these commands.
 // (Use 5a4aa8fb-0f1b-43a0-b81c-d5218a642b58 instead of mulle-data if there are duplicate entries)
-//    remove:             `mulle-sourcetree mark mulle-data no-header`
+//    remove:             `mulle-sde dependency mark mulle-data no-header`
 //    rename:             `mulle-sde dependency|library set mulle-data include whatever.h`
-//    reorder:            `mulle-sourcetree move mulle-data <up|down>`
-//    toggle #include:    `mulle-sourcetree mark mulle-data [no-]import`
-//    toggle public:      `mulle-sourcetree mark mulle-data [no-]public`
-//    toggle optional:    `mulle-sourcetree mark mulle-data [no-]require`
-//    remove for platform:`mulle-sourcetree mark mulle-data no-platform-<uname>`
-//        (use mulle-sourcetree-to-c --unames for known values)
+//    reorder:            `mulle-sde dependency move mulle-data <up|down>`
+//    toggle #include:    `mulle-sde dependency mark mulle-data [no-]import`
+//    toggle public:      `mulle-sde dependency mark mulle-data [no-]public`
+//    toggle optional:    `mulle-sde dependency mark mulle-data [no-]require`
+//    remove for platform:`mulle-sde dependency mark mulle-data no-platform-<uname>`
+//        (use `mulle-sourcetree-to-c --unames` to list known values)
 #include <mulle-data/mulle-data.h>   // mulle-data
 
 #endif
