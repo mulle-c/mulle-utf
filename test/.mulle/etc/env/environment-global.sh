e16e58e0a9f893c14870a1569cf3848b21a8a646
Nat! <nat@mulle-kybernetik.com>
2026-04-12 13:14:27 +0200

    move windows cross-compile vars from test/global to user-host scope

--- test/.mulle/etc/env/environment-global.sh
+++ test/.mulle/etc/env/environment-global.sh
@@ -7,19 +7,7 @@ export MULLE_FETCH_SEARCH_PATH="${MULLE_FETCH_SEARCH_PATH}:${MULLE_VIRTUAL_ROOT}
 #
 #
 #
-export MULLE_CRAFT_PLATFORMS="${MULLE_UNAME}:windows"
 
-
-#
-#
-#
-export MULLE_SOURCETREE_PLATFORMS="${MULLE_UNAME}:windows"
-
-
-#
-#
-#
-export MULLE_CRAFT_TOOLCHAIN__WINDOWS="toolchain--linux-windows--x86_64-w64-mingw32--mulle-clang"
 
 
 #
@@ -31,12 +19,24 @@ export MULLE_CRAFT_TOOLCHAIN__WINDOWS="toolchain--linux-windows--x86_64-w64-ming
 #
 #
 #
-export MULLE_CRAFT_CROSS_COMPILER_ROOT__WINDOWS="/opt/mulle-clang-project-windows/latest"
 
 
+
 #
 #
 #
-export MULLE_EMULATOR__WINDOWS="WINEDEBUG=fixme-all,err-all wine"
+
+
+
+#
+#
+#
+
+
+
+#
+#
+#
+
 
 
